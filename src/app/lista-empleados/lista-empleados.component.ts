1f66be1d26bd984a2760ec2ea8c91483b2440a07
Santiago Salas <santiagosalas.6532@gmail.com>
2022-04-18 20:52:53 -0500

    method subscribe to list backend

--- src/app/lista-empleados/lista-empleados.component.ts
+++ src/app/lista-empleados/lista-empleados.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Empleado } from '../empleado';
+import { EmpleadoService } from '../empleado.service';
 
 @Component({
   selector: 'app-lista-empleados',
@@ -10,21 +11,17 @@ export class ListaEmpleadosComponent implements OnInit {
 
   empleados:Empleado[]
 
-  constructor() { }
+  constructor(private empleadoServicio : EmpleadoService) { }
 
   ngOnInit(): void {
-    this.empleados = [{
-      "id" : 1,
-      "name" : "Santiago",
-      "lastName" : "Salas",
-      "email" : "santiagosalas.6532@gmail.com"
-    },
-    {
-      "id" : 2,
-      "name" : "Valentina",
-      "lastName" : "Munoz",
-      "email" : "valentina.m.6532@gmail.com"
-    }];
+    this.obtenerEmpleados();
+  }
+
+  //Metodo subscrito a todo el listado, y al aarreglo se le inicializaran los datos que se vayan obteniendo
+  private obtenerEmpleados() {
+    this.empleadoServicio.obtenerListaDeEmpleados().subscribe(dato => {
+      this.empleados = dato;
+    });
   }
 
 }
